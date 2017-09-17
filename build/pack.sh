7ebfc714625d5c479c7205acac7522ec167cf971
Danny Lin <danny0838@gmail.com>
2017-09-17 10:05:36 +0800

    build: add packer scripts

---
+++ build/pack.sh
@@ -0,0 +1,27 @@
+# System requirements:
+# * OS: *nix
+# * zip
+#
+# Steps:
+# * Adjust $filename variable to fit your needs.
+# * Run this script, and the packed files are created in the ../dist directory.
+#
+#
+filename="content-farm-terminator"
+dir=$(dirname $(realpath "$0"))
+src=$(realpath "$dir/../src")
+dist=$(realpath "$dir/../dist")
+cd "$src"
+
+# Chrome extension package (for submit)
+fn="$filename.zip" &&
+rm "$dist/$fn" &&
+zip -r "$dist/$fn" * -x '.git*' &&
+zip -d "$dist/$fn" 'manifest-firefox.json'
+
+# Firefox addon
+fn="$filename.xpi" &&
+rm "$dist/$fn" &&
+zip -r "$dist/$fn" * -x '.git*' &&
+zip -d "$dist/$fn" 'manifest.json' &&
+printf "@ manifest-firefox.json\n@=manifest.json\n" | zipnote -w "$dist/$fn"
