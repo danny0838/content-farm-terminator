2f79cfaba5bde5aa280a3f0b9d57ae6ebc1e7f61
Danny Lin <danny0838@gmail.com>
2017-09-29 00:42:17 +0800

    mark: support encrypted.google.com

--- src/content.js
+++ src/content.js
@@ -13,7 +13,7 @@ function getRedirectedUrlOrHostname(elem) {
 
     // Google
     // adopted from WOT: http://static-cdn.mywot.com/settings/extensions/serps.json
-    if (/^(www\.)?(google)\.([a-z]{2,3})(\.[a-z]{2,3})?$/.test(h)) {
+    if (/^(www\.|encrypted\.)?(google)\.([a-z]{2,3})(\.[a-z]{2,3})?$/.test(h)) {
       if (p === "/url" || p === "/interstitial") {
         return s.get("url") || s.get("q");
       }
