f1e458050f7b7ee0651420cf1c389572cc4d3cc7
Danny Lin <danny0838@gmail.com>
2017-11-07 15:51:08 +0800

    core: fix wildcards(*) not working

--- src/utils.js
+++ src/utils.js
@@ -260,7 +260,7 @@ class ContentFarmFilter {
         reRuleSet.add(rule);
       } else {
         // standard rule
-        rule = utils.escapeRegExp(ruleText).replace(/\\\*/g, "[^:/?#\[\]]*");
+        rule = utils.escapeRegExp(ruleText).replace(/\\\*/g, "[^:/?#]*");
         ruleSet.add(rule);
       }
     });
