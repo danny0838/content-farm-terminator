eb2a56428a8fd419aaa6b4cadc157f2a16dc8e85
Danny Lin <danny0838@gmail.com>
2021-05-17 21:50:21 +0800

    core: refactor code

--- src/background.js
+++ src/background.js
@@ -244,179 +244,199 @@ function onBeforeRequestBlocker(details) {
  * This will be replaced by onBeforeRequestBlocker as long as updateFilter
  * is done.
  */
-let onBeforeRequestCallback = function (details) {
+function onBeforeRequestCallback(details) {
   return updateFilterPromise.then(() => {
     return onBeforeRequestBlocker(details);
   });
 };
 
-chrome.webRequest.onBeforeRequest.addListener((details) => {
-  return onBeforeRequestCallback(details);
-}, {urls: ["*://*/*"], types: ["main_frame", "sub_frame"]}, ["blocking"]);
+function initBeforeRequestListener() {
+  chrome.webRequest.onBeforeRequest.addListener((details) => {
+    return onBeforeRequestCallback(details);
+  }, {urls: ["*://*/*"], types: ["main_frame", "sub_frame"]}, ["blocking"]);
+}
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  // console.warn("omMessage", message);
-  const {cmd, args} = message;
-  switch (cmd) {
-    case 'isUrlBlocked': {
-      updateFilterPromise.then(() => {
-        const blockType = filter.isBlocked(args.url);
-        sendResponse(blockType);
-      });
-      return true; // async response
-      break;
-    }
-    case 'isTempUnblocked': {
-      sendResponse(tempUnblockTabs.has(sender.tab.id));
-      break;
-    }
-    case 'tempUnblock': {
-      const tabId = sender.tab.id;
-      utils.getOptions([
-        "tempUnblockDuration",
-        "tempUnblockCountdownBase",
-        "tempUnblockCountdownIncrement",
-        "tempUnblockCountdownReset",
-        "tempUnblockCountdown",
-        "tempUnblockLastAccess",
-      ]).then((options) => {
-        // temporarily unblock the tab
-        tempUnblockTabs.add(tabId);
-        setTimeout(() => {
-          tempUnblockTabs.delete(tabId);
-        }, options.tempUnblockDuration);
+function initMessageListener() {
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    // console.warn("omMessage", message);
+    const {cmd, args} = message;
+    switch (cmd) {
+      case 'isUrlBlocked': {
+        updateFilterPromise.then(() => {
+          const blockType = filter.isBlocked(args.url);
+          sendResponse(blockType);
+        });
+        return true; // async response
+        break;
+      }
+      case 'isTempUnblocked': {
+        sendResponse(tempUnblockTabs.has(sender.tab.id));
+        break;
+      }
+      case 'tempUnblock': {
+        const tabId = sender.tab.id;
+        utils.getOptions([
+          "tempUnblockDuration",
+          "tempUnblockCountdownBase",
+          "tempUnblockCountdownIncrement",
+          "tempUnblockCountdownReset",
+          "tempUnblockCountdown",
+          "tempUnblockLastAccess",
+        ]).then((options) => {
+          // temporarily unblock the tab
+          tempUnblockTabs.add(tabId);
+          setTimeout(() => {
+            tempUnblockTabs.delete(tabId);
+          }, options.tempUnblockDuration);
+          sendResponse(true);
+
+          // update countdown
+          if (options.tempUnblockLastAccess < 0 ||
+              Date.now() - options.tempUnblockLastAccess > options.tempUnblockCountdownReset) {
+            options.tempUnblockCountdown = -1;
+          }
+
+          if (options.tempUnblockCountdown === -1) {
+            options.tempUnblockCountdown = options.tempUnblockCountdownBase;
+          }
+
+          options.tempUnblockCountdown += options.tempUnblockCountdownIncrement;
+          options.tempUnblockLastAccess = Date.now();
+
+          return utils.setOptions({
+            tempUnblockCountdown: options.tempUnblockCountdown,
+            tempUnblockLastAccess: options.tempUnblockLastAccess,
+          });
+        });
+        return true; // async response
+        break;
+      }
+      case 'getMergedBlacklist': {
+        updateFilterPromise.then(() => {
+          sendResponse(filter.getMergedBlacklist());
+        });
+        return true; // async response
+        break;
+      }
+      case 'updateOptions': {
+        const validator = new ContentFarmFilter();
+        args.transformRules = validator.validateTransformRulesText(args.transformRules);
+        validator.addTransformRules(args.transformRules);
+        args.userBlacklist = validator.validateRulesText(args.userBlacklist, 'url');
+        args.userWhitelist = validator.validateRulesText(args.userWhitelist, 'url');
+        utils.setOptions(args).then(() => {
+          sendResponse(true);
+        });
+        return true; // async response
+        break;
+      }
+      case 'closeTab': {
+        Promise.resolve().then(() => {
+          if (args.tabId) { return [args.tabId]; }
+          return new Promise((resolve, reject) => {
+            chrome.tabs.query({
+              active: true,
+              currentWindow: true
+            }, resolve);
+          }).then((tabs) => {
+            return tabs.map(x => x.id);
+          });
+        }).then((tabIds) => {
+          return new Promise((resolve, reject) => {
+            chrome.tabs.remove(tabIds, resolve);
+          });
+        });
         sendResponse(true);
-
-        // update countdown
-        if (options.tempUnblockLastAccess < 0 ||
-            Date.now() - options.tempUnblockLastAccess > options.tempUnblockCountdownReset) {
-          options.tempUnblockCountdown = -1;
-        }
-
-        if (options.tempUnblockCountdown === -1) {
-          options.tempUnblockCountdown = options.tempUnblockCountdownBase;
-        }
-
-        options.tempUnblockCountdown += options.tempUnblockCountdownIncrement;
-        options.tempUnblockLastAccess = Date.now();
-
-        return utils.setOptions({
-          tempUnblockCountdown: options.tempUnblockCountdown,
-          tempUnblockLastAccess: options.tempUnblockLastAccess,
-        });
-      });
-      return true; // async response
-      break;
-    }
-    case 'getMergedBlacklist': {
-      updateFilterPromise.then(() => {
-        sendResponse(filter.getMergedBlacklist());
-      });
-      return true; // async response
-      break;
-    }
-    case 'updateOptions': {
-      const validator = new ContentFarmFilter();
-      args.transformRules = validator.validateTransformRulesText(args.transformRules);
-      validator.addTransformRules(args.transformRules);
-      args.userBlacklist = validator.validateRulesText(args.userBlacklist, 'url');
-      args.userWhitelist = validator.validateRulesText(args.userWhitelist, 'url');
-      utils.setOptions(args).then(() => {
-        sendResponse(true);
-      });
-      return true; // async response
-      break;
-    }
-    case 'closeTab': {
-      Promise.resolve().then(() => {
-        if (args.tabId) { return [args.tabId]; }
-        return new Promise((resolve, reject) => {
-          chrome.tabs.query({
-            active: true,
-            currentWindow: true
-          }, resolve);
-        }).then((tabs) => {
-          return tabs.map(x => x.id);
-        });
-      }).then((tabIds) => {
-        return new Promise((resolve, reject) => {
-          chrome.tabs.remove(tabIds, resolve);
-        });
-      });
-      sendResponse(true);
-      break;
-    }
-  }
-});
-
-chrome.storage.onChanged.addListener((changes, areaName) => {
-  // Config keys are stored in storage.sync and fallbacks to storage.local;
-  // cache keys are stored in storage.local and are valid JSON format.
-  // We only take action when at least one config key is changed.
-  if (areaName !== "sync") {
-    // skip if it's a storage.local.remove() rather than a real user modification
-    for (let key in changes) {
-      if (!("newValue" in changes[key])) { return; }
-      break;
-    }
-
-    // skip if no config key is changed
-    try {
-      for (let key in changes) { JSON.parse(key); }
-      return;
-    } catch(ex) {}
-  }
-
-  if ("showContextMenuCommands" in changes || "quickContextMenuCommands" in changes) {
-    const options = [];
-    if ("showContextMenuCommands" in changes) { options.push("showContextMenuCommands"); }
-    if ("quickContextMenuCommands" in changes) { options.push("quickContextMenuCommands"); }
-    contextMenuController.refresh(options);
-  }
-
-  if ("suppressHistory" in changes) {
-    historyController.refresh();
-  }
-
-  updateFilter().then(() => {
-    // @TODO:
-    // Say we have a shift from local to sync:
-    //
-    //     local {webBlacklists: "list1\nlist2"} => sync {webBlacklists: "list1"}
-    //     sync  {webBlacklists: ""}
-    //
-    // We get a change of sync: "" => "list1" and a change of local: "list1\nlist2" => undefined,
-    // and the cache of list2 is not cleared, while it should be, leaving staled cache not cleared.
-    if (changes.webBlacklists) {
-      filter.clearStaleWebListCache(changes.webBlacklists);
+        break;
+      }
     }
   });
-});
+}
 
-chrome.runtime.onInstalled.addListener((details) => {
-  const {reason, previousVersion} = details;
+function initStorageChangeListener() {
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    // Config keys are stored in storage.sync and fallbacks to storage.local;
+    // cache keys are stored in storage.local and are valid JSON format.
+    // We only take action when at least one config key is changed.
+    if (areaName !== "sync") {
+      // skip if it's a storage.local.remove() rather than a real user modification
+      for (let key in changes) {
+        if (!("newValue" in changes[key])) { return; }
+        break;
+      }
 
-  if (reason === "update" && utils.versionCompare(previousVersion, "2.1.2") === -1) {
-    return Promise.resolve().then(() => {
-      console.warn("Migrating options from < 2.1.2");
-      return utils.getOptions({
-        "webBlacklist": undefined,
-        "webBlacklists": undefined,
-      }).then((options) => {
-        if (options.webBlacklist && (typeof options.webBlacklists === "undefined")) {
-          let newWebBlacklists = utils.defaultOptions.webBlacklists + "\n" + options.webBlacklist;
-          return utils.setOptions({webBlacklists: newWebBlacklists}).then(() => {
-            updateFilter();
-          });
-        }
-      });
-    }).then(() => {
-      console.warn("Migrated successfully.");
+      // skip if no config key is changed
+      try {
+        for (let key in changes) { JSON.parse(key); }
+        return;
+      } catch(ex) {}
+    }
+
+    if ("showContextMenuCommands" in changes || "quickContextMenuCommands" in changes) {
+      const options = [];
+      if ("showContextMenuCommands" in changes) { options.push("showContextMenuCommands"); }
+      if ("quickContextMenuCommands" in changes) { options.push("quickContextMenuCommands"); }
+      contextMenuController.refresh(options);
+    }
+
+    if ("suppressHistory" in changes) {
+      historyController.refresh();
+    }
+
+    updateFilter().then(() => {
+      // @TODO:
+      // Say we have a shift from local to sync:
+      //
+      //     local {webBlacklists: "list1\nlist2"} => sync {webBlacklists: "list1"}
+      //     sync  {webBlacklists: ""}
+      //
+      // We get a change of sync: "" => "list1" and a change of local: "list1\nlist2" => undefined,
+      // and the cache of list2 is not cleared, while it should be, leaving staled cache not cleared.
+      if (changes.webBlacklists) {
+        filter.clearStaleWebListCache(changes.webBlacklists);
+      }
     });
-  }
-});
+  });
+}
 
-if (chrome.browserAction) {
+function initInstallListener() {
+  chrome.runtime.onInstalled.addListener((details) => {
+    const {reason, previousVersion} = details;
+
+    if (reason === "update" && utils.versionCompare(previousVersion, "2.1.2") === -1) {
+      return Promise.resolve().then(() => {
+        console.warn("Migrating options from < 2.1.2");
+        return utils.getOptions({
+          "webBlacklist": undefined,
+          "webBlacklists": undefined,
+        }).then((options) => {
+          if (options.webBlacklist && (typeof options.webBlacklists === "undefined")) {
+            let newWebBlacklists = utils.defaultOptions.webBlacklists + "\n" + options.webBlacklist;
+            return utils.setOptions({webBlacklists: newWebBlacklists}).then(() => {
+              updateFilter();
+            });
+          }
+        });
+      }).then(() => {
+        console.warn("Migrated successfully.");
+      });
+    }
+  });
+}
+
+function initBrowserAction() {
+  if (!chrome.browserAction) {
+    // Firefox Android < 55: no browserAction
+    // Fallback to pageAction.
+    // Firefox Android ignores the tabId parameter and
+    // shows the pageAction for all tabs
+    chrome.pageAction.onClicked.addListener((tab) => {
+      const url = chrome.runtime.getURL("options.html");
+      chrome.tabs.create({url: url, active: true});
+    });
+    chrome.pageAction.show(0);
+  }
+
   chrome.browserAction.onClicked.addListener((tab) => {
     let url;
     try {
@@ -431,26 +451,26 @@ if (chrome.browserAction) {
     }
     chrome.tabs.create({url: url, active: true});
   });
-} else {
-  // Firefox Android < 55: no browserAction
-  // Fallback to pageAction.
-  // Firefox Android ignores the tabId parameter and
-  // shows the pageAction for all tabs
-  chrome.pageAction.onClicked.addListener((tab) => {
-    const url = chrome.runtime.getURL("options.html");
-    chrome.tabs.create({url: url, active: true});
-  });
-  chrome.pageAction.show(0);
 }
 
-contextMenuController.refresh();
-historyController.refresh();
+function init() {
+  initBeforeRequestListener();
+  initMessageListener();
+  initStorageChangeListener();
+  initInstallListener();
+  initBrowserAction();
 
-updateFilter().then(() => {
-  onBeforeRequestCallback = onBeforeRequestBlocker;
-  return utils.getOptions([
-    "webBlacklistsUpdateInterval",
-  ]).then((options) => {
-    setInterval(updateFilter, options.webBlacklistsUpdateInterval);
+  contextMenuController.refresh(); // async
+  historyController.refresh(); // async
+
+  updateFilter().then(() => {
+    onBeforeRequestCallback = onBeforeRequestBlocker;
+    return utils.getOptions([
+      "webBlacklistsUpdateInterval",
+    ]).then((options) => {
+      setInterval(updateFilter, options.webBlacklistsUpdateInterval);
+    });
   });
-});
+}
+
+init();
