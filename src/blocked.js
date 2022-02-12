b07297c5924acc7893b3a48bd1734adc1030e649
Danny Lin <danny0838@gmail.com>
2022-02-12 16:20:00 +0800

    ui: fix occasional block page error
    - This happens when recheckBlock() returns faster than DOMContentLoaded event.

--- src/blocked.js
+++ src/blocked.js
@@ -17,6 +17,47 @@ function recheckBlock() {
     if (!isBlocked) {
       location.replace(sourceUrl);
     }
+    return isBlocked;
+  });
+}
+
+function autoUpdateUnblockButton() {
+  return utils.getOptions([
+    "showUnblockButton",
+    "tempUnblockCountdownBase",
+    "tempUnblockCountdownReset",
+    "tempUnblockCountdown",
+    "tempUnblockLastAccess",
+  ]).then((options) => {
+    if (!options.showUnblockButton) {
+      return;
+    }
+
+    if (options.tempUnblockLastAccess < 0 ||
+        Date.now() - options.tempUnblockLastAccess > options.tempUnblockCountdownReset) {
+      options.tempUnblockCountdown = -1;
+    }
+
+    if (options.tempUnblockCountdown === -1) {
+      options.tempUnblockCountdown = options.tempUnblockCountdownBase;
+    }
+
+    let countdown = options.tempUnblockCountdown;
+    const elem = document.querySelector('#unblock');
+    elem.hidden = false;
+    elem.disabled = true; // Firefox might record status causing non-disabled
+    elem.textContent = utils.lang("unblockBtnCountdown", [countdown / 1000]);
+
+    let t = setInterval(() => {
+      countdown -= 1000;
+      if (countdown > 0) {
+        elem.textContent = utils.lang("unblockBtnCountdown", [countdown / 1000]);
+      } else {
+        clearInterval(t);
+        elem.textContent = utils.lang("unblockBtn");
+        elem.disabled = false;
+      }
+    }, 1000);
   });
 }
 
@@ -49,40 +90,46 @@ function onBackClick() {
 function init() {
   utils.loadLanguages(document);
 
-  try {
-    const sourceUrlObj = new URL(sourceUrl);
-    if (!(sourceUrlObj.protocol === 'http:' || sourceUrlObj.protocol === 'https:')) {
-      throw new Error('URL not under http(s) protocol.');
+  // in case that sourceUrl is already unblocked
+  return recheckBlock().then((isBlocked) => {
+    if (!isBlocked) {
+      return;
     }
-    if (urlObj.searchParams.get('type') == 2) {
-      const url = utils.getNormalizedUrl(sourceUrlObj);
+
+    try {
+      const sourceUrlObj = new URL(sourceUrl);
+      if (!(sourceUrlObj.protocol === 'http:' || sourceUrlObj.protocol === 'https:')) {
+        throw new Error('URL not under http(s) protocol.');
+      }
+      if (urlObj.searchParams.get('type') == 2) {
+        const url = utils.getNormalizedUrl(sourceUrlObj);
+        const elem = document.createElement('span');
+        elem.textContent = url;
+        elem.style.fontSize = '0.8em';
+        document.querySelector('#warningUrl').appendChild(elem);
+      } else {
+        document.querySelector('#warningUrl').textContent = punycode.toASCII(sourceUrlObj.hostname);
+      }
+    } catch (ex) {
+      // sourceUrl is invalid, show raw sourceUrl
+      // this should not happen unless the user manually enters the URL
       const elem = document.createElement('span');
-      elem.textContent = url;
+      elem.textContent = sourceUrl;
       elem.style.fontSize = '0.8em';
       document.querySelector('#warningUrl').appendChild(elem);
-    } else {
-      document.querySelector('#warningUrl').textContent = punycode.toASCII(sourceUrlObj.hostname);
     }
-  } catch (ex) {
-    // sourceUrl is invalid, show raw sourceUrl
-    // this should not happen unless the user manually enters the URL
-    const elem = document.createElement('span');
-    elem.textContent = sourceUrl;
-    elem.style.fontSize = '0.8em';
-    document.querySelector('#warningUrl').appendChild(elem);
-  }
 
-  document.querySelector('#detailsLink').href = `options.html?from=${encodeURIComponent(sourceUrl)}`;
+    document.querySelector('#detailsLink').href = `options.html?from=${encodeURIComponent(sourceUrl)}`;
 
-  // Firefox might record status and make it non-disabled
-  document.querySelector('#unblock').disabled = true;
+    /**
+     * Events
+     */
+    document.querySelector('#view').addEventListener('click', onViewClick);
+    document.querySelector('#unblock').addEventListener('click', onUnblockClick);
+    document.querySelector('#back').addEventListener('click', onBackClick);
 
-  /**
-   * Events
-   */
-  document.querySelector('#view').addEventListener('click', onViewClick);
-  document.querySelector('#unblock').addEventListener('click', onUnblockClick);
-  document.querySelector('#back').addEventListener('click', onBackClick);
+    autoUpdateUnblockButton(); // async
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
@@ -97,44 +144,4 @@ browser.runtime.onMessage.addListener((message, sender) => {
       return Promise.resolve(true);
     }
   }
-});
-
-// in case that sourceUrl is already unblocked
-recheckBlock().then(() => {
-  return utils.getOptions([
-    "showUnblockButton",
-    "tempUnblockCountdownBase",
-    "tempUnblockCountdownReset",
-    "tempUnblockCountdown",
-    "tempUnblockLastAccess",
-  ]).then((options) => {
-    if (!options.showUnblockButton) {
-      return;
-    }
-
-    if (options.tempUnblockLastAccess < 0 ||
-        Date.now() - options.tempUnblockLastAccess > options.tempUnblockCountdownReset) {
-      options.tempUnblockCountdown = -1;
-    }
-
-    if (options.tempUnblockCountdown === -1) {
-      options.tempUnblockCountdown = options.tempUnblockCountdownBase;
-    }
-
-    let countdown = options.tempUnblockCountdown;
-    const elem = document.querySelector('#unblock');
-    elem.hidden = false;
-    elem.textContent = utils.lang("unblockBtnCountdown", [countdown / 1000]);
-
-    let t = setInterval(() => {
-      countdown -= 1000;
-      if (countdown > 0) {
-        elem.textContent = utils.lang("unblockBtnCountdown", [countdown / 1000]);
-      } else {
-        clearInterval(t);
-        elem.textContent = utils.lang("unblockBtn");
-        elem.disabled = false;
-      }
-    }, 1000);
-  });
 });
